e5eb4971a7a0bcad449243d511c4760af7c60903
Amit Joe <90870511+Amit-Joe-coder@users.noreply.github.com>
2022-10-22 11:46:54 +0530

    Create MDA-4.c
    4. Write a program in C to find the sum of right diagonals of a matrix.
    The Above program helps to find the right diagonal of a matrix ,However matrix must be of same rows and columns i.e. n x m, Where n=m.

---
+++ MDA-4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int sum = 0, x = 100, y = 100, arr1[x][y], i, j;
+    printf("Enter the number of rows:");
+    scanf("%d", &x);
+    printf("Enter the number of columns: ");
+    scanf("%d", &y);
+    for (i = 0; i < x; i++)
+    {
+        for (j = 0; j < y; j++)
+        {
+            scanf("%d", &arr1[i][j]);
+        }
+    } // This print the matrix.
+    printf("Matrix that you are using:\n");
+    for (i = 0; i < x; i++)
+    {
+        for (j = 0; j < y; j++)
+        {
+            printf("%d ", arr1[i][j]);
+        }
+        printf("\n");
+    } // This is for finding the right diagonal.Here is main logic of this program.
+    for (i = 0; i < x; i++)
+    {
+        for (j = 0; j <= y; j++)
+        {
+            if ((i) < (j + 1))
+            {
+                sum = arr1[i][j] + sum;
+                printf("%d", arr1[i][j]);
+                printf("+");
+                break;
+            }
+        }
+        printf("\n");
+    }
+    printf("%d", sum);
+}
